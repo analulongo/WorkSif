5c89d1a74474c9abe49caf27318f1fcd0fe4bf97
felipe <smurfef@hotmail.com>
2013-07-06 11:28:44 -0300

    12

--- src/java/dao/EmprestimoDAO.java
+++ src/java/dao/EmprestimoDAO.java
@@ -4,10 +4,60 @@
  */
 package dao;
 
-/**
- *
- * @author Felipe
- */
+import java.util.List;
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
+import model.Emprestimo;
+
+@Stateless
 public class EmprestimoDAO {
-    
+
+    @PersistenceContext(unitName = "SistemaPU")
+    private EntityManager em;
+
+    public boolean gravar(Emprestimo emprestimo) {
+        boolean sucesso = false;
+        try {
+            em.merge(emprestimo);
+            sucesso = true;
+        } catch (Exception e) {
+        }
+
+        return sucesso;
+    }
+
+    public Emprestimo selecionar(Long codigo) {
+        Emprestimo emprestimo = null;
+        try {
+            emprestimo = em.find(Emprestimo.class, codigo);
+        } catch (Exception e) {
+        }
+
+        return emprestimo;
+    }
+
+    public boolean remover(Emprestimo emprestimo) {
+        boolean sucesso = false;
+        try {
+            emprestimo = em.find(Emprestimo.class, emprestimo.getCodigo());
+            em.remove(emprestimo);
+            sucesso = true;
+        } catch (Exception e) {
+        }
+
+        return sucesso;
+    }
+
+    public List<Emprestimo> listar() {
+        List<Emprestimo> emprestimos = null;
+        try {
+            Query query = em.createQuery("Select c from emprestimo c");
+            emprestimos = query.getResultList();
+        } catch (Exception e) {
+        }
+
+        return emprestimos;
+    }
 }
